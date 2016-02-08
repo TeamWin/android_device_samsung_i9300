2b79bc693ed71782d717ac902b27f950a60f3a1b
SdtBarbarossa <DaMicha_essen@gmx.de>
2016-02-08 19:08:13 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9300/bluetooth
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-TARGET_KERNEL_CONFIG := device/samsung/i9300/kernelconfig/cyanogenmod_i9300_defconfig
+TARGET_KERNEL_CONFIG := ../../../../../../device/samsung/i9300/kernelconfig/cyanogenmod_i9300_defconfig
 BOARD_RIL_CLASS := ../../../device/samsung/i9300/ril
 
 # Recovery
